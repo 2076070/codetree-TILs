41068ee270893a5ebf570d76d14624fe3f4d214d
YeIn kim <103043743+2076070@users.noreply.github.com>
2024-09-22 14:37:22 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 269ms, Memory: 108MB, Status: Passed - CodeTree

--- 240922/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240922/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -10,36 +10,25 @@ result = 0
 
 # 첫번째 사각형
 x1, y1, x2, y2 = map(int, input().split())
-for i in range(x1, x2):
-    for j in range(y1, y2):
+for i in range(x1, x2): # -2 ~ 1 = -2 -1 0
+    for j in range(y1, y2): # -2 ~ 1 = -2 -1 0
         arr[i+offset][j+offset] = 1
 
 # 두번째 사각형
 x1, y1, x2, y2 = map(int, input().split())
-for i in range(x1, x2):
-    for j in range(y1, y2):
+for i in range(x1, x2): # 0 ~ 3 = 0 1 2
+    for j in range(y1, y2): # 0 1 2
         arr[i+offset][j+offset] = 0
 
-a1, a2, b1, b2 = (0, 0, 0, 0)
-# 좌측하단 끝점
-searching = True
-for i in range(0, 2000):
-    for j in range(0, 2000):
-        if((arr[i][j]==1) and (searching==True)):
-            a1, b1 = (i-offset, j-offset) # (2,1)
-            searching = False
+x_list = []
+y_list = []
 
-# 우측상단 끝점
-searching = True
-for i in range(1999, -1, -1):
-    for j in range(1999, -1, -1):
-        if((arr[i][j]==1) and (searching==True)):
-            a2, b2 = (i-offset+1, j-offset+1)
-            searching = False
+for i in range(0, 2000) :
+    for j in range(0, 2000) :
+        if (arr[i][j] == 1) :
+            x_list.append(i-offset)
+            y_list.append(j-offset)
 
-# test
-# print(a1, b1, a2, b2)
-
-# 양단 끝점으로 정사각형 넓이 출력하기
-result = abs(a2-a1) * abs(b2-b1)
+if (x_list and y_list): # 둘 다 비어있지 않을 것
+    result = (max(x_list) - min(x_list) + 1) * (max(y_list) - min(y_list) + 1)
 print(result)
