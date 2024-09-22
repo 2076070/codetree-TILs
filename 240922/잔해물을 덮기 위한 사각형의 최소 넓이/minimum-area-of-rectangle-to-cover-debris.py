b6290435affe02eca6c46a6995e126e1a2567bf2
YeIn kim <103043743+2076070@users.noreply.github.com>
2024-09-22 14:02:02 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 252ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240922/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240922/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -20,26 +20,26 @@ for i in range(x1, x2):
     for j in range(y1, y2):
         arr[i+offset][j+offset] = 0
 
+a1, a2, b1, b2 = (0, 0, 0, 0)
 # 좌측하단 끝점
 searching = True
 for i in range(0, 2000):
     for j in range(0, 2000):
         if((arr[i][j]==1) and (searching==True)):
-            x1, y1 = (i-offset, j-offset) # (2,1)
+            a1, b1 = (i-offset, j-offset) # (2,1)
             searching = False
-
 
 # 우측상단 끝점
 searching = True
 for i in range(1999, -1, -1):
     for j in range(1999, -1, -1):
         if((arr[i][j]==1) and (searching==True)):
-            x2, y2 = (i-offset+1, j-offset+1)
+            a2, b2 = (i-offset+1, j-offset+1)
             searching = False
 
 # test
-# print(x1, y1, x2, y2)
+# print(a1, b1, a2, b2)
 
 # 양단 끝점으로 정사각형 넓이 출력하기
-result = abs(x2-x1) * abs(y2-y1)
+result = abs(a2-a1) * abs(b2-b1)
 print(result)
