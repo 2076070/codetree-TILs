fb88dd73722528866976fcde5678347b7f4461aa
YeIn kim <103043743+2076070@users.noreply.github.com>
2024-10-01 20:17:09 +0900

    Title: 체크판위에서 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241001/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,21 @@
+R, C = map(int, input().split())
+arr = [
+    list(input().split())
+    for _ in range(R)
+]
+
+# 조건에 맞는 경로 세기
+cnt = 0
+
+for i in range(1, R):
+    for j in range(1, C):
+        if (arr[0][0] != arr[i][j]):
+            for ii in range(i+1, R):
+                for jj in range(j+1, C):
+                    if(arr[i][j] != arr[ii][jj]):
+                        for iii in range(ii+1, R):
+                            for jjj in range(jj+1, C):
+                                if(arr[ii][jj] != arr[iii][jjj]):
+                                    cnt+=1
+
+print(cnt)
